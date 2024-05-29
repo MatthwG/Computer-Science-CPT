d8d99bf292ceda3b2ac3b43e07b3d21c4b1d81aa
MatthwG <149129569+MatthwG@users.noreply.github.com>
2024-05-28 22:09:26 -0400

    Backup Pushed (pls don't corrupt it)

---
+++ NewCorrectGAMEPLSDONTCORRUPT/Assets/Code/Attack.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Attack : MonoBehaviour
+{
+    public GameObject ProjectilePrefab;
+    public GameObject ProjectilePrefab2;
+    public Movement Movey;
+    public ParticleSystem particle;
+    public ParticleSystem particle1;
+    public float Speed = 4.5f;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position += transform.right * Time.deltaTime*Speed;
+        
+        
+    }
+/*
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.tag == "All")
+        {
+            
+            Destroy(gameObject);
+            
+            
+        }
+        
+    } */
+}
